ecda75e55a6f55e5ff431ea82f7dc9ae8d4cdd07
Affaled <affaledl@gmail.com>
2023-12-23 20:40:20 -0300

    import header

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
 <template>
+  <Header />
   <router-view />
 </template>
-<script></script>
+<script>
+import { defineComponent } from "vue";
+import Header from "./components/Header.vue";
+export default defineComponent({
+  components: { Header },
+});
+</script>
