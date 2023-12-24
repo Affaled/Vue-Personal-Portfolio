819ba1190d82023c503c87ddf2b112f52e79a52e
Affaled <affaledl@gmail.com>
2023-12-23 21:03:41 -0300

    font awesome adicionada

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./Routes.js";
+import "https://kit.fontawesome.com/002f302af0.js";
 import "./style.css";
 
 createApp(App).use(router).mount("#app");
