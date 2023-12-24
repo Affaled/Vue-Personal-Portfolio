20b56ba542f7a78b3a3b327edfe4d2d2b13cf3b8
Affaled <affaledl@gmail.com>
2023-12-24 15:27:24 -0300

    data profissional adicionada

--- src/pages/Biography.vue
+++ src/pages/Biography.vue
@@ -1,3 +1,25 @@
 <template>
-  <div></div>
+  <section
+    class="flex min-h-screen w-full flex-col items-center justify-start pb-12 pt-32"
+  >
+    <h2 class="text-center font-serif text-7xl capitalize">Biography</h2>
+    <div>
+      <h3 class="text-start font-serif text-3xl">Front-end developer</h3>
+      <p class="text-start">{{ BiographyData.professionalInfo[0] }}</p>
+      <p class="text-start">{{ BiographyData.professionalInfo[1] }}</p>
+      <p class="text-start">{{ BiographyData.professionalInfo[2] }}</p>
+    </div>
+  </section>
 </template>
+
+<script>
+import BiographyData from "../data/BiographyData.js";
+import { defineComponent } from "vue";
+export default defineComponent({
+  data() {
+    return {
+      BiographyData,
+    };
+  },
+});
+</script>
