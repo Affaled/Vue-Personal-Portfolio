d87f2ef278376b0557ca6ea57a85c201797e6708
Affaled <affaledl@gmail.com>
2023-12-24 12:55:11 -0300

    banner da home pronto

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,4 +1,15 @@
-<template></template>
+<template>
+  <section
+    class="pt-18 flex min-h-screen w-full flex-col items-center justify-end gap-4 pb-12"
+  >
+    <h2 class="font-serif text-7xl capitalize">Front-end developer</h2>
+    <p
+      class="cursor-pointer text-sm font-semibold uppercase hover:text-[#e4cb86]"
+    >
+      Scroll down
+    </p>
+  </section>
+</template>
 <script>
 import { defineComponent } from "vue";
 export default defineComponent({});
