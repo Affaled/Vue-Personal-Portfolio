936d1feb330dc6b9db2e647b8e3a4e51e59ee734
Affaled <affaledl@gmail.com>
2023-12-24 14:23:44 -0300

    transition adicionada

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,27 +5,45 @@
         <router-link to="/">Affaled</router-link>
       </h1>
       <nav class="text-sm font-semibold">
-        <ul class="flex flex-row gap-8">
-          <li class="uppercase hover:text-[#e4cb86]">
-            <router-link to="/">Home</router-link>
+        <ul class="flex w-full flex-row items-center gap-8">
+          <li>
+            <router-link
+              to="/"
+              class="uppercase transition duration-150 hover:text-[#e4cb86]"
+              >Home</router-link
+            >
           </li>
-          <li class="uppercase hover:text-[#e4cb86]">
-            <router-link to="/biography">Biography</router-link>
+          <li>
+            <router-link
+              to="/biography"
+              class="uppercase transition duration-150 hover:text-[#e4cb86]"
+              >Biography</router-link
+            >
           </li>
-          <li class="uppercase hover:text-[#e4cb86]">
-            <router-link to="/projects">Projects</router-link>
+          <li>
+            <router-link
+              to="/projects"
+              class="uppercase transition duration-150 hover:text-[#e4cb86]"
+              >Projects</router-link
+            >
           </li>
         </ul>
       </nav>
       <div class="flex flex-row gap-4 text-3xl">
         <a href="https://github.com/affaled" target="_blank">
-          <i class="fab fa-github hover:text-[#e4cb86]"></i>
+          <i
+            class="fab fa-github transition duration-150 hover:text-[#e4cb86]"
+          ></i>
         </a>
         <a href="https://instagram.com/affaled.dev" target="_blank">
-          <i class="fab fa-instagram hover:text-[#e4cb86]"></i>
+          <i
+            class="fab fa-instagram transition duration-150 hover:text-[#e4cb86]"
+          ></i>
         </a>
         <a href="https://linkedin.com/in/lucas-affaled" target="_blank">
-          <i class="fab fa-linkedin hover:text-[#e4cb86]"></i>
+          <i
+            class="fab fa-linkedin transition duration-150 hover:text-[#e4cb86]"
+          ></i>
         </a>
       </div>
     </div>
