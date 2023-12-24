76b9166304663c2b5b77f98312c01c4536341a66
Affaled <affaledl@gmail.com>
2023-12-24 14:49:02 -0300

    text center

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,7 +2,9 @@
   <section
     class="flex min-h-screen w-full flex-col items-center justify-end gap-4 pb-12 pt-20"
   >
-    <h2 class="font-serif text-7xl capitalize">Front-end developer</h2>
+    <h2 class="text-center font-serif text-7xl capitalize">
+      Front-end developer
+    </h2>
     <p
       class="cursor-pointer text-sm font-semibold uppercase hover:text-[#e4cb86]"
     >
