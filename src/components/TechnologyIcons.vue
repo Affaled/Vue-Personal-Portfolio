4cd8501959fc78066eb64863e430effa9c6bfa06
Affaled <affaledl@gmail.com>
2023-12-25 19:16:14 -0300

    criação de componente para renderizar os svgs

---
+++ src/components/TechnologyIcons.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-row gap-2">
+    <img
+      v-for="(iconName, index) in iconNames"
+      :key="index"
+      :src="getItemSrc(iconName)"
+      class="h-10 w-10"
+      :alt="iconName + ' icon'"
+    />
+  </div>
+</template>
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+  props: {
+    iconNames: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    getItemSrc(iconName) {
+      return `@/assets/svg/technology/${iconName}.svg`;
+    },
+  },
+});
+</script>
