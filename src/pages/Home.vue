3ad35e95ac02beedef9b092f839edd87a19497dc
Affaled <affaledl@gmail.com>
2023-12-25 22:37:52 -0300

    biography section in home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,3 +1,4 @@
+p
 <template>
   <section
     class="flex min-h-screen w-full flex-col items-center justify-end gap-4 pb-12 pt-20"
@@ -12,11 +13,12 @@
     </p>
   </section>
   <section
-    class="flex w-full flex-col items-center justify-end gap-4 pb-12 pt-20"
+    class="flex w-full flex-col items-center justify-end gap-4 py-20"
+    id="bio"
   >
     <div class="flex w-11/12 max-w-5xl flex-col gap-4">
       <p
-        class="w-full text-start font-sans text-lg font-semibold capitalize text-[#e4cb86]"
+        class="mb-8 w-full text-start font-sans text-lg font-semibold capitalize text-[#e4cb86]"
       >
         Biography & Technology
       </p>
@@ -37,6 +39,22 @@
             ]"
           />
         </div>
+        <div class="flex w-1/2 flex-col gap-4">
+          <p>
+            {{ BiographyData.personalInfo[0] }}
+          </p>
+          <p>
+            {{ BiographyData.professionalInfo[1] }}
+          </p>
+          <router-link
+            to="/biography"
+            @click.native="scrollToTop"
+            class="flex w-max cursor-pointer flex-row items-center gap-4 border-b-2 border-slate-200 text-sm font-semibold uppercase tracking-widest hover:border-[#e4cb86] hover:text-[#e4cb86]"
+          >
+            <p>Learn More</p>
+            <i class="fas fa-arrow-right"></i>
+          </router-link>
+        </div>
       </div>
     </div>
   </section>
@@ -44,7 +62,18 @@
 <script>
 import { defineComponent } from "vue";
 import TechnologyIcons from "../components/TechnologyIcons.vue";
+import BiographyData from "../data/BiographyData.js";
 export default defineComponent({
   components: { TechnologyIcons },
+  data() {
+    return {
+      BiographyData,
+    };
+  },
+  methods: {
+    scrollToTop() {
+      window.scrollTo(0, 0);
+    },
+  },
 });
 </script>
