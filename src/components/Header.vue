4c17ef6b84aa745988dfd4d736a821d8de0e26af
Affaled <affaledl@gmail.com>
2023-12-29 07:11:32 -0300

    esboço do header feito

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,45 @@
-<template></template>
 <script>
 import { defineComponent } from "vue";
-export default defineComponent({});
+export default defineComponent({
+  data() {
+    return {
+      LanguagesOpen: false,
+    };
+  },
+});
 </script>
+
+<template>
+  <header class="flex h-28 w-full items-center justify-center">
+    <div class="flex h-full w-10/12 items-center justify-between">
+      <div class="flex flex-col items-center font-serif text-3xl">
+        <router-link to="/" class="text-center uppercase hover:text-[#e4cb86]">
+          Aff
+          <br />
+          led
+        </router-link>
+      </div>
+      <div class="flex items-center gap-8 text-lg font-light">
+        <nav>
+          <ul class="flex gap-8">
+            <li class="uppercase">
+              <router-link to="/projects" class="hover:text-[#e4cb86]"
+                >Projects</router-link
+              >
+            </li>
+            <li class="uppercase">
+              <router-link to="/biography" class="hover:text-[#e4cb86]"
+                >Biography</router-link
+              >
+            </li>
+          </ul>
+        </nav>
+        <p
+          class="flex cursor-pointer items-center gap-1 uppercase hover:text-[#e4cb86]"
+        >
+          En <i class="fas fa-angle-down"></i>
+        </p>
+      </div>
+    </div>
+  </header>
+</template>
