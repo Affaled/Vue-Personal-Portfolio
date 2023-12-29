5036e83bcfc593e010c60772e07c2949a2972a84
Affaled <affaledl@gmail.com>
2023-12-29 07:28:48 -0300

    absolute adicionado no aside

--- src/components/ContactAside.vue
+++ src/components/ContactAside.vue
@@ -4,7 +4,9 @@ export default defineComponent({});
 </script>
 
 <template>
-  <aside class="flex w-1/12 flex-col items-end justify-end gap-6 text-2xl">
+  <aside
+    class="absolute flex h-screen w-1/12 flex-col items-end justify-end gap-6 pb-20 text-2xl"
+  >
     <i class="fa fa-github"></i>
     <i class="fa fa-linkedin"></i>
     <i class="fa fa-instagram"></i>
