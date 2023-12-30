26be790057f14316bcc9e9bb48630981d695fc74
Affaled <affaledl@gmail.com>
2023-12-30 20:25:52 -0300

    Home feita

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,13 @@
-<template></template>
 <script>
 import { defineComponent } from "vue";
 export default defineComponent({});
 </script>
+
+<template>
+  <div class="flex w-10/12 flex-col items-center justify-center">
+    <div class="flex flex-col items-end">
+      <h1 class="font-serif text-9xl uppercase">Affaled</h1>
+      <p class="text-2xl capitalize">Front-end developer</p>
+    </div>
+  </div>
+</template>
