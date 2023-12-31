fa5735afb3c4edc06f138a66ee5766a681beb372
Affaled <affaledl@gmail.com>
2023-12-30 21:15:28 -0300

    página projects esboço em andamento

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,5 +1,29 @@
-<template></template>
 <script>
 import { defineComponent } from "vue";
 export default defineComponent({});
 </script>
+
+<template>
+  <div class="absolute top-0 flex h-screen w-screen justify-center">
+    <div class="relative mb-20 mt-28 flex w-10/12 items-end">
+      <div class="flex h-4/5 flex-col justify-start gap-12">
+        <div class="flex flex-col gap-6">
+          <p class="font-serif text-7xl capitalize">Project Name</p>
+          <ul class="flex gap-4 text-2xl uppercase">
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Angular</li>
+          </ul>
+        </div>
+        <div class="ml-32 w-1/3">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam,
+            fugit numquam quia aliquid natus saepe praesentium quo voluptatibus
+            sint beatae nam! Natus iure molestias id aperiam, est similique
+            minus quibusdam.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
