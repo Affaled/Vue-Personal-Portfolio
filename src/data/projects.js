41460901b275f604b05d9362d3131f63ee45a97e
Affaled <affaledl@gmail.com>
2024-01-08 09:44:09 -0300

    troca de json para js expostando um json

---
+++ src/data/projects.js
@@ -0,0 +1,22 @@
+const projects = {
+  projects: [
+    {
+      id: 1,
+      name: "Blog",
+      technologies: ["Angular", "Typescript", "SCSS"],
+      description: "Angular Blog",
+      repository: "https://www.github.com/",
+      url: "https://www.github.com/",
+    },
+    {
+      id: 2,
+      name: "Prototype Portfolio",
+      technologies: ["ReactJS", "Javascript", "SCSS"],
+      description: "ReactJS Portfolio",
+      repository: "https://www.github.com/",
+      url: "https://www.github.com/",
+    },
+  ],
+};
+
+export default projects;
